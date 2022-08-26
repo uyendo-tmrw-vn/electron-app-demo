dc00937db456e3a44a5c7455ee0e529b942beb05
UyenDo <>
2022-08-26 15:41:00 +0700

    Push code electron

---
+++ renderer/data/index.js
@@ -0,0 +1,12 @@
+import HomeJson from './home.json';
+import WorksJson from './works.json';
+import timeslotJson from './timeslot.json';
+import studioJson from './studio.json';
+import durationJson from './duration.json';
+import calendarBookingJson from './calendarBooking.json';
+import equimentPriceJson from './equimentPrice.json';
+
+export {
+    HomeJson, WorksJson, timeslotJson, studioJson, durationJson, calendarBookingJson,
+    equimentPriceJson
+}
---
+++ renderer/pages/booking/Readme.md
@@ -0,0 +1,8 @@
+```
+1. Filling all field 
+2. Click "Payment" button
+3. Open Modal "Your booking is created"
+4. Click "Payment" button on modal (check CheckPaymentStatus each 60s) => to Osime Payment (new tab)
+5. When payment success to show modal "Your booking is completed."
+```
+
